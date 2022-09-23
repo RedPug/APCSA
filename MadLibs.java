66c6b8e2294e03a8a2c53cef63e325367f55259c
Richards <rowanr34355@stu.powayusd.com>
2022-09-23 14:09:24 -0700

    finalized comments & added a loop to keep running madlibs

--- MadLibs.java
+++ MadLibs.java
@@ -2,7 +2,17 @@ import java.util.Random;
 import java.util.Scanner;
 
 /**
- * MadLibs project
+ * 1.2.10 MadLibs
+ * Rowan Richards, Harper Hakim
+ * 21 september, 2022
+ * 
+ * Picks a random riddle from a list and prompts the user
+ * to fill it outwithout knowing the original statement
+ * 
+ * Preconditions:
+ * Riddle to fill in, user input through the console
+ * 
+ * Postconditions: a printed statement of the riddle filled in with user inputs
  */
 
 public class MadLibs {
@@ -15,36 +25,43 @@ public class MadLibs {
             "If not for the <noun>, the <adjective> and <adjective> elephant would go extinct.",
             "As you <verb> up to the <adjective> house, you knock on the door to hear a <otomatopoeia/sound>."
         };
+
         Random rand = new Random(); //for getting random numbers
 
-        String riddle = riddles[(int)(rand.nextInt(riddles.length))]; //pick a random riddle from the list
+        Scanner sc = new Scanner(System.in); //for getting user inputs
 
-        Scanner sc = new Scanner(System.in);
 
-        //while a <> section exists, repeat
-        while(riddle.indexOf('<') != -1){
-            int i = riddle.indexOf('<');//index of the start of the first "word"
-            int j = riddle.indexOf('>');//index of the end of the first "word"
+        //in order to keep promting new MadLibs
+        boolean running = true;
 
-            //if(i > j)break;
-            String thing = riddle.substring(i + 1, j);//the stuff inside <>
+        while(running){
+            String riddle = riddles[(int)(rand.nextInt(riddles.length))]; //pick a random riddle from the list
 
-            //asks for the thing, using an if needed
-            if(thing.startsWith("a") || thing.startsWith("e") || thing.startsWith("o") || thing.startsWith("i") || thing.startsWith("u")){
-                System.out.println("Enter an " + thing);
-            }else{
-                System.out.println("Enter a " + thing);
-            }
+            //while a <> section exists, keep asking for something
+            while(riddle.indexOf('<') != -1){
+                int i = riddle.indexOf('<');//index of the start of the first "word"
+                int j = riddle.indexOf('>');//index of the end of the first "word"
     
-            String input = sc.nextLine();//gets a user input
-            
-            //generates new text with the user input replacing the <> and stuff inside it, and the rest of the riddle remaining
-            String new_riddle = riddle.substring(0,i)+input+riddle.substring(j+1);
-            
-            //sets the current riddle being iterated on to the new riddle
-            riddle = new_riddle;
+                if(i > j) break; //if the open bracket comes after the closed bracket, stop running.
+                //running with i > j will result in the substring method throwing an exception
+    
+                String thing = riddle.substring(i + 1, j);//the stuff inside <>, what to ask for
+    
+                //asks for the thing, using "an" if needed
+                if(thing.startsWith("a") || thing.startsWith("e") || thing.startsWith("o") || thing.startsWith("i") || thing.startsWith("u")){
+                    System.out.print("Enter an " + thing + ": ");
+                }else{
+                    System.out.print("Enter a " + thing + ": ");
+                }
+        
+                String input = sc.nextLine();//gets a user input
+                if(input.equals("end")){running = false; break;}
+                
+                //replaces the <...> of the riddle with the user input
+                riddle = riddle.substring(0,i)+input+riddle.substring(j+1);
+            }
+            System.out.println(riddle); 
         }
-
-        System.out.println(riddle);
+        
     }
 }
